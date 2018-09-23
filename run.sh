2076ffa1c5ddfa5ee03eae85848d19eb9fcefcaf
umdez <leofelippedias@hotmail.com.br>
2018-09-23 16:00:20 -0300

    Now even if we cant remove our docker container we will still runing the second comand.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker rm -f mysql5.7 && docker run --detach --name=mysql5.7 --env="MYSQL_ROOT_PASSWORD=mypassword" mysql:5.7
+sudo docker rm -f mysql5.7; docker run --detach --name=mysql5.7 --env="MYSQL_ROOT_PASSWORD=mypassword" mysql:5.7
